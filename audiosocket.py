8ec596dd057a365605339eacb18a153d8b214de9
anatoly techtonik <techtonik@gmail.com>
2011-07-01 21:19:12 +0300

    starting at least something

---
+++ audiosocket.py
@@ -0,0 +1,9 @@
+"""
+Implementation of Raw Audio Socket server spec in pure Python
+http://code.google.com/p/rainforce/wiki/RawAudioSocket
+
+"""
+
+#-- CHAPTER 1: CONTINUOUS SOUND PLAYBACK WITH WINDOWS WINMM LIBRARY
+
+#-- /CHAPTER 1
