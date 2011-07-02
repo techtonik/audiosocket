1446d8431367976d0e3cdba9ca64904671e6f876
anatoly techtonik <techtonik@gmail.com>
2011-07-02 08:26:09 +0300

    first step - try to open and close media device

--- audiosocket.py
+++ audiosocket.py
@@ -4,6 +4,20 @@ http://code.google.com/p/rainforce/wiki/RawAudioSocket
 
 """
 
-#-- CHAPTER 1: CONTINUOUS SOUND PLAYBACK WITH WINDOWS WINMM LIBRARY
 
-#-- /CHAPTER 1
+#-- CHAPTER 1: CONTINUOUS SOUND PLAYBACK WITH WINDOWS WINMM LIBRARY --
+#
+# Based on tutorial "Playing Audio in Windows using waveOut Interface"
+# by David Overton
+
+import ctypes
+
+# 1. Open Sound Device
+
+# mmsystem.h
+from ctypes import wintypes
+
+HWAVEOUT = wintypes.HANDLE
+
+
+#-- /CHAPTER 1 --
